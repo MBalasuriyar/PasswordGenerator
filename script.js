926748755d864d7b9ddd6f7265b24f817df9e5d7
MBalasuriyar <michaelbalasuriyar@gmail.com>
2021-04-10 22:51:01 -0400

    working now

--- script.js
+++ script.js
@@ -1,73 +1,97 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var length = 0
+var askLower = false
+var askNumber = false
+var askSymbol = false
+var askUpper = false
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-
-}
-
+//opption strings
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
 var lowerCase = ["abcdefghijklmnopqrstuvwxyz"]
 var numbers = ["0123456789"]
 var symbols = ["!#$%&()*+-/:;<=>?@[]~_`^"]
 
-var fullStringForSearch = ""
 
+//To be Concatonated string
+var fullStringForSearch = ""
+//Password from string
 var generated = ""
 
 
+
+//generation function
 function generatePassword() {
-  // length = prompt("How long should the password be?  Only 8-12")
-  // for (length < 8 || legth > 12 ) {
-  //  alert(" Only 8 - 12 !")
-  //  generatePassword()
-  // }
-
-  askUpper = confirm("Do you want uppercase letters in the password")
-
-  askNumber = confirm("Do you want numbers in the password")
-  askLower = confirm("Do you want lowercase letters in the password")
-
-  askSymbol = confirm("Do you want special characters in the password")
+    //reset string
+fullStringForSearch = ""
 
 
-  if (!askSymbol && !askNumber && !askUpper && !askLower) {
-    alert("pick at least one!")
-    generatePassword()
-  }
+ askLower = false
+ askNumber = false
+ askSymbol = false
+ askUpper = false
 
-  if (askSymbol) {
-    fullStringForSearch = symbols;
-    console.log(fullStringForSearch)
-  }
-  if (askNumber) {
-    fullStringForSearch += numbers;
-    console.log(fullStringForSearch)
-  }
-  if (askUpper) {
-    fullStringForSearch += upperCase;
-    console.log(fullStringForSearch)
-  }
-  if (askLower) {
-    fullStringForSearch += lowerCase;
-    console.log(fullStringForSearch)
-  }
+    // Check and store Password length
+    var length = Number(prompt("How long should the password be?  Only 8-12"));
+    if (length < 8 || length > 12) {
+        alert(" Only 8 - 12 !");
+        generatePassword();
+    }
 
-  if (i = 0, i < length.length, i++)  {
+    //Confirm different options
+    askUpper = confirm("Do you want uppercase letters in the password")
+    askNumber = confirm("Do you want numbers in the password")
+    askLower = confirm("Do you want lowercase letters in the password")
+    askSymbol = confirm("Do you want special characters in the password")
 
-    generated + fullStringForSearch[(Math.floor(Math.random()))]
-    console.log(generated)
-    i++
+    //check and add confirmations
 
+    if (!askSymbol && !askNumber && !askUpper && !askLower) {
+        alert("pick at least one!")
+        generatePassword()
+    }
 
-  }
+    if (askSymbol) {
+        fullStringForSearch += symbols;
+        console.log(fullStringForSearch)
+    }
+    if (askNumber) {
+        fullStringForSearch += numbers;
+        console.log(fullStringForSearch)
+    }
+    if (askUpper) {
+        fullStringForSearch += upperCase;
+        console.log(fullStringForSearch)
+    }
+    if (askLower) {
+        fullStringForSearch += lowerCase;
+        console.log(fullStringForSearch)
+    }
+
+    for (var i = 0; i < length; i++) {
+        console.log("what")
+        generated += fullStringForSearch[(Math.floor(Math.random() * fullStringForSearch.length))]
+        console.log(generated)
+        console.log(fullStringForSearch[(Math.floor(Math.random() * fullStringForSearch.length))])
+
+    }
+    return generated
 
 }
 
+
+//supplied code
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+
+}
